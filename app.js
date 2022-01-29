4ff3141295dcd0c2da20e867b564f28990a99e3c
Fahisakollath <93871075+Fahisakollath@users.noreply.github.com>
2022-01-29 14:47:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+///////////////////////////////////
+const cors = require('cors')
+var session = require('express-session')
+
+
+/////////////////////////////////////
+const db = require('./Connection/Connection')
+
+db.connect(() => {
+        console.log("database connected sucessfully")
+    })
+    //////////////////////////////////////
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+/////////////////////////////////////
+// these two are use in middlware section this portion is the middleware section
+
+
+app.use(cors())
+    //////////////////////////////
+app.use(session({
+        secret: 'password',
+        // resave: false,
+        // saveUninitialized: true,
+        cookie: { maxAge: 10000 }
+    }))
+    ///////////////////////////////////
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,307 @@
+var express = require('express');
+var router = express.Router();
+
+const session = require('express-session');
+
+
+
+////////////////////////////////////////////////////
+const { ObjectId } = require('mongodb');
+
+const db = require('../Connection/Connection')
+    /////////////////////////////////////////////////////////
+    /* GET home page. */
+
+let values = {
+
+}
+const verification = (req, res, next) => {
+    console.log("user verif", values);
+    values.signed ? next() : res.json({ message: "section time out" })
+
+}
+
+
+router.post('/login', async function(req, res, next) {
+    console.log(req.body);
+    console.log("reg", req.session)
+    req.session.signed = req.body
+        // console.log("req", req.session.signed);
+    values = req.session.signed
+    console.log("final", values);
+
+
+    const result = await db.get().collection('login').findOne({ email: req.body.email })
+
+
+    if (result === null) {
+        res.json({ message: 'No user found' });
+    } else {
+        const pword = await db.get().collection('login').findOne({ password: req.body.password })
+
+        if (pword === null) {
+            res.json({ message: 'password or email incorrect' });
+
+        } else {
+
+            // res.json({ message: 'Login sucessfully' });
+            const typee = await db.get().collection('login').findOne({ email: req.body.email })
+
+            console.log("finall", values);
+            values.signed = true
+
+
+            // console.log("typeec", typee)
+            res.json(typee)
+
+
+        }
+
+
+    }
+});
+
+
+router.post('/take_membership', async function(req, res, next) {
+    console.log("fffff", req.body)
+
+    const result = await db.get().collection('users').findOne({ email: req.body.email } && { dob: req.body.dob })
+    const resultt = await db.get().collection('membership_request').findOne({ email: req.body.email } && { dob: req.body.dob })
+    console.log("result", result);
+    console.log("result2", resultt);
+
+    if (result === null && resultt === null) {
+        db.get().collection('membership_request').insertOne(req.body)
+        res.json({ message: 'sent request to membership' });
+
+    } else {
+        res.json({ message: 'Already request to membership' });
+    }
+
+
+
+
+});
+
+
+router.post('/create_account', function(req, res, next) {
+    console.log("fffff", req.body)
+    if (req.body.password !== req.body.confirmpassword) {
+
+        res.json({ message: 'password does not match' });
+    } else {
+        db.get().collection('login').insertOne(req.body)
+        res.json({ message: 'create account sucessfully' });
+    }
+
+});
+
+
+
+router.post('/add_books', verification, async function(req, res, next) {;
+    console.log("nnn");
+    console.log("fffff", req.body)
+
+    const result = await db.get().collection('book_details').find({ bookname: req.body.bookdetails.bookname, author: req.body.bookdetails.author, edition: req.body.bookdetails.edition, language: req.body.bookdetails.language }).toArray()
+    console.log("result", result);
+    // console.log("bookname", req.body.bookname);
+
+
+    if (result.length === 0) {
+        await db.get().collection('book_details').insertOne({ bookname: req.body.bookdetails.bookname, author: req.body.bookdetails.author, image: req.body.url, edition: req.body.bookdetails.edition, language: req.body.bookdetails.language, category: req.body.bookdetails.category, stock: req.body.bookdetails.stock, arrangement: 'pending' })
+        res.json({ message: 'insert sucessfully' });
+
+    } else {
+        // console.log("stock", result.stock);
+
+        await db.get().collection('book_details').updateMany({ bookname: req.body.bookname, author: req.body.author, edition: req.body.edition, language: req.body.language }, { $set: { stock: parseInt(result.stock) + parseInt(req.body.stock) } })
+
+        res.json({ message: 'Already exists' });
+    }
+
+
+});
+
+
+router.get('/viewbookdetails', async function(req, res, next) {
+    const data = await db.get().collection('book_details').find().toArray()
+    console.log("dataaaa", data)
+
+    res.json(data);
+
+
+});
+
+router.get('/viewrequest', async function(req, res, next) {
+    console.log("haiiii")
+    const data = await db.get().collection('membership_request').find({ type: "subadmin" }).toArray()
+    console.log("dataaaa", data)
+
+    res.json(data);
+
+
+});
+
+router.post('/acceptreq', async function(req, res, next) {
+    console.log("iddd", req.body);
+    const result = await db.get().collection('membership_request').find({ _id: ObjectId(req.body.id), status: "Accepted" }).toArray()
+
+
+    if (result.length !== 0) {
+        console.log(result);
+        res.json({ message: 'Already accepted' });
+
+    } else {
+        console.log(result);
+        await db.get().collection('membership_request').updateOne({ _id: ObjectId(req.body.id) }, { $set: { status: "Accepted" } })
+        res.json({ message: 'request accepted' });
+    }
+
+
+});
+
+
+router.get('/userrequest', async function(req, res, next) {
+    console.log("haiiii")
+    const data = await db.get().collection('membership_request').find({ type: "user" }).toArray()
+    console.log("dataaaa", data)
+
+    res.json(data);
+
+
+});
+
+
+router.post('/subacceptreq', async function(req, res, next) {
+    console.log("iddd", req.body);
+    const result = await db.get().collection('membership_request').find({ _id: ObjectId(req.body.id), status: "Accepted" }).toArray()
+
+
+    if (result.length !== 0) {
+        console.log(result);
+        res.json({ message: 'Already accepted' });
+
+    } else {
+        console.log(result);
+        await db.get().collection('membership_request').updateOne({ _id: ObjectId(req.body.id) }, { $set: { status: "Accepted" } })
+        res.json({ message: 'request accepted' });
+    }
+
+
+});
+
+router.get('/arrange', async function(req, res, next) {
+    const data = await db.get().collection('book_details').find().toArray()
+    console.log("dataaaa", data)
+
+    res.json(data);
+
+
+});
+
+router.post('/arrangebook"', async function(req, res, next) {
+    console.log("iddd", req.body);
+    const result = await db.get().collection('book_details').find({ _id: ObjectId(req.body.id), status: "Accepted" }).toArray()
+
+
+    if (result.length !== 0) {
+        console.log(result);
+        res.json({ message: 'Already accepted' });
+
+    } else {
+        console.log(result);
+        await db.get().collection('membership_request').updateOne({ _id: ObjectId(req.body.id) }, { $set: { status: "Accepted" } })
+        res.json({ message: 'request accepted' });
+    }
+
+
+});
+
+//////////////////issue
+router.post('/book_arrangement', async function(req, res, next) {
+    console.log("rrr", req.body);
+    const result = await db.get().collection('book_arrangment').find({ bookid: ObjectId(req.body.id) }).toArray()
+    console.log("result", result);
+    if (result.length === null) {
+
+        await db.get().collection('book_arrangment').insertOne({ bookid: ObjectId(req.body.id), arrangedbook: req.body.stock, referanceid: req.body.bookarrange.referanceid, shelfno: req.body.bookarrange.shelfno })
+        await db.get().collection('book_details').updateOne({ _id: ObjectId(req.body.id) }, { $set: { arrangement: "arranged" } })
+        res.json({ message: 'arranged' });
+
+
+    } else {
+        await db.get().collection('book_arrangment').updateOne({ _id: ObjectId(req.body.id) }, { $set: { arrangedbook: parseInt(result.arrangedbook) + parseInt(req.body.arrangedbook) } })
+
+        res.json({ message: 'arranged once again' });
+    }
+
+
+});
+
+
+
+
+
+router.get('/searchbooks', async function(req, res, next) {
+    // const data = await db.get().collection('book_details').find().toArray()
+    // const result = await db.get().collection('book_arrangment').find().toArray()
+
+    const result = await db.get().collection('book_arrangment').aggregate([{
+        $lookup: {
+            from: 'book_details',
+            localField: 'bookid',
+            foreignField: '_id',
+
+            as: 'bookdetails'
+        }
+    }, {
+
+
+
+        $project: {
+            _id: 0,
+            bookid: 0,
+            referanceid: 0,
+            arrangedbook: 0,
+            // shelfno: 1,
+            // bookdetails: 1
+        }
+
+    }]).toArray()
+
+
+
+    console.log("dataaaa", result)
+        // console.log("datbookdetails", result[0])
+        // console.log("dddddd", result[0].shelfno);
+        // console.log("ccc", result[0].bookdetails[0].bookname);
+
+    res.json(result);
+
+
+});
+
+
+
+// router.get('/searchbooks', async function(req, res, next) {
+//     const data = await db.get().collection('book_details').find().toArray()
+//     console.log("dataaaa", data)
+
+//     res.json(data);
+
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
